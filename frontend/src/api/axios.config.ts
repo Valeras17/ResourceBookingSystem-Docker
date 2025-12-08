8b163fb15664348a565b7f51192db95d63770cea
Valeras17 <valerius89gord@gmail.com>
2025-12-08 14:24:56 +0200

    change for bk render

--- frontend/src/api/axios.config.ts
+++ frontend/src/api/axios.config.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 
 // Используем переменную окружения или захардкоженный URL для production
-const API_URL = import.meta.env.VITE_API_URL || 'https://resourcebookingsystem-docker-1.onrender.com/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://resourcebookingsystem-docker.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
